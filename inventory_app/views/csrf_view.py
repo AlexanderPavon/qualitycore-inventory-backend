bff196b9cbd70c2bd2c59b6ba22f06306f9cd9f4
Alexander <a.alexanderpavon@gmail.com>
2025-08-10 23:24:15 -0500

    Prueba4

--- inventory_app/views/csrf_view.py
+++ inventory_app/views/csrf_view.py
@@ -1,5 +1,7 @@
-# inventory_app/views/csrf_view.py
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
+
 @ensure_csrf_cookie
-def csrf_ready(request): return JsonResponse({"detail":"ok"})
+def csrf_ready(request):
+    return JsonResponse({"detail": "ok", "csrfToken": get_token(request)})
